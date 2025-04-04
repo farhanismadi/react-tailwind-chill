d23c905fa14e9cac762df47ad540a3ea2e3e5b53
farhanismadi <muhammadfarhanismadi@gmail.com>
2025-04-04 21:11:49 +0700

    feat: adding routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,37 @@
+import { useRoutes } from "react-router-dom";
+import { Login } from "../views/Login";
+import { Register } from "../views/Register";
+import { Homepage } from "../views/homepage";
+import { NotFound } from "../views/NotFound";
+import { Layout } from "../layout/Layout";
+
+const router = () => [
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+export const Routes = (props) => {
+  const routers = router(props);
+  let routes = useRoutes(routers);
+  return routes;
+};
